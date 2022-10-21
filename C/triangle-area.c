3a1974b2e63d5405835c4ac44105d2636fea3511
liviagomes30 <liviagomes30@gmail.com>
2022-10-21 11:05:15 -0300

    hacktoberfest contribution

---
+++ C/triangle-area.c
@@ -0,0 +1,25 @@
+// Name: Lívia Gomes
+
+// Username: liviagomes30
+
+// Favorite Language: Javascript 
+
+#include<stdio.h>
+
+/* C code to calculate the area of a triangle */
+
+int main ()
+{
+    float base, height, area;
+
+    printf ("Enter the value of the triangle base:  ");
+    scanf("%f", &base);
+
+    printf ("Enter the value of the height of the triangle:  ");
+    scanf("%f", &height);
+
+    area = (base*height)/2;
+
+    printf("Area: %f", area);
+    return 0;
+}
