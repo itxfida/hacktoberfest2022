1284d25d575dae2624f2d111f4b524203d7786f1
iam_chandra3 <chandrasekhar_v@srmap.edu.in>
2022-10-24 13:57:43 +0530

    Added Binary sort in C++

---
+++ C++/binarysort.cpp
@@ -0,0 +1,40 @@
+//Binary sort in C++
+#include <iostream>
+
+using namespace std;
+
+void binarySort(int arr[], int n)
+{
+
+    for (int i = 1; i < n; i++)
+    {
+
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    binarySort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
